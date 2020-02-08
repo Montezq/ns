9e83964563b18268e5c15e05ae783ee3a152bb29
gavani <intalmaci21@gmail.com>
2020-02-08 21:59:44 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.header__navbar-menu-icon').click(function(){
+		$(this).toggleClass('open');
+		$('.header__menu').toggleClass('open');
+		$('.section').toggleClass('blur');
+		$('body').toggleClass('menu_open');
+	});
+	
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uKCl7XHJcblx0JCgnLmhlYWRlcl9fbmF2YmFyLW1lbnUtaWNvbicpLmNsaWNrKGZ1bmN0aW9uKCl7XHJcblx0XHQkKHRoaXMpLnRvZ2dsZUNsYXNzKCdvcGVuJyk7XHJcblx0XHQkKCcuaGVhZGVyX19tZW51JykudG9nZ2xlQ2xhc3MoJ29wZW4nKTtcclxuXHRcdCQoJy5zZWN0aW9uJykudG9nZ2xlQ2xhc3MoJ2JsdXInKTtcclxuXHRcdCQoJ2JvZHknKS50b2dnbGVDbGFzcygnbWVudV9vcGVuJyk7XHJcblx0fSk7XHJcblx0XHJcbn0pOyJdfQ==
